1c0a9b5f5ba90552252ee5078c17363f6db43cb8
Roger <rdpender3@gmail.com>
2018-10-09 18:40:14 -0700

    push before class

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newsscraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gamescraper";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Handlebars
 app.engine(
